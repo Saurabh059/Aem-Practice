a122cc2a03ac31aabcb16295931addafb89f68f6
Saurabh059 <gupta99999saurabh@gmail.com>
2016-05-29 13:58:36 +0530

    new project setup

---
+++ bundle/src/test/java/com/practice/mobile/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.practice.mobile;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
